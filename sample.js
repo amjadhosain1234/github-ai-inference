03fb6b289837ef4c9f3a77ef1201693238fe9d38
Jamal Uddin <140267521+amjadhosain1234@users.noreply.github.com>
2025-03-31 00:45:19 +0600

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,24 +1,31 @@
 import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
-const token = process.env["MY_GITHUB_TOKEN"];
+
+const token = process.env["GITHUB_TOKEN"];
 const endpoint = "https://models.inference.ai.azure.com";
 const modelName = "gpt-4o";
 
 export async function main() {
-  const client = new OpenAI({ baseURL: endpoint, apiKey: token });
+  const client = ModelClient(
+    endpoint,
+    new AzureKeyCredential(token),
+  );
 
-  const response = await client.chat.completions.create({
-    messages: [
-        { role:"system", content: "You are a helpful assistant." },
+  const response = await client.path("/chat/completions").post({
+    body: {
+      messages: [
         { role:"user", content: "What is the capital of France?" }
       ],
-      temperature: 1.0,
-      top_p: 1.0,
       max_tokens: 1000,
       model: modelName
-    });
+    }
+  });
 
-  console.log(response.choices[0].message.content);
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+
+  console.log(response.body.choices[0].message.content);
 }
 
 main().catch((err) => {
